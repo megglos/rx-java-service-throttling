3df393033affde287a5106bc71bdc6507728a1ef
Sebastian Bathke <sb@hubrick.com>
2018-02-27 10:15:28 +0100

    wip

--- src/main/java/io/vertx/example/rxjava/http/ThrottledServer.java
+++ src/main/java/io/vertx/example/rxjava/http/ThrottledServer.java
@@ -9,31 +9,39 @@ import rx.Observable;
 import rx.Single;
 import rx.schedulers.Schedulers;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class ThrottledServer extends AbstractVerticle {
     private static final Logger log = LoggerFactory.getLogger(ThrottledServer.class);
 
     private AtomicInteger activeExecutions = new AtomicInteger(0);
 
-    private int maxExecutionsPerInterval = 5;
+    private int maxExecutionsPerInterval = 1000;
     private int timeIntervalSeconds = 1;
 
     public static void main(String[] args) {
         Runner.runExample(ThrottledServer.class);
     }
 
+    private List<Integer> values = IntStream.range(0,10000).boxed().collect(toList());
+
     @Override
     public void start() throws Exception {
         HttpServer server = vertx.createHttpServer();
         server.requestStream().toObservable().subscribe(req -> {
             log.info("Got request");
-            executeThrottled(doStuff())
+            Observable.from(values)
+                    .flatMap(value -> executeThrottled(Single.defer(() -> doStuff(value))).toObservable())
+                    .toList()
                     // can use timeout to limit pressure
 //                    .timeout(2, TimeUnit.SECONDS)
                     .subscribe(
-                            responseText -> req.response().end(responseText),
+                            responseText -> req.response().end(responseText.toString()),
                             throwable -> {
                                 log.error(throwable.getMessage(), throwable);
                                 req.response().setStatusCode(500).end(throwable.toString());
@@ -43,8 +51,8 @@ public class ThrottledServer extends AbstractVerticle {
         server.listen(8080);
     }
 
-    public Single<String> doStuff() {
-        return Single.just("Hey");
+    public Single<String> doStuff(final Integer value) {
+        return Single.just("Hey" + value).doOnEach(notification -> log.info(notification.getValue()));
     }
 
     public <T> Single<T> executeThrottled(final Single<T> toExecute) {
